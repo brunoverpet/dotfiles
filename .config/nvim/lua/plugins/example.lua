8480db33d942f46521ebaf7b0ee1a4f45144e4ab
brunoverpet <bverpet46@gmail.com>
2026-06-28 18:39:15 +0200

    chore(nvim): reset to clean LazyVim starter

---
+++ .config/nvim/lua/plugins/example.lua
@@ -0,0 +1,13 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+if true then return {} end
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+--
+-- If you want to pick a specific colorscheme, set it via LazyVim options:
+-- LazyVim.setup({ colorscheme = "gruvbox" })
